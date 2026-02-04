613f15febfa21a2dd22c0f3ee808f3463a9a8e24
ruthgabriellep <153202236+ruthgabriellep@users.noreply.github.com>
2026-02-04 17:31:46 +0000

    Added companion

---
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.5f;
+
+    private GameObject player;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}
