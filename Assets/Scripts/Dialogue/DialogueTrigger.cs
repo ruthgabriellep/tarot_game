74035a8cec5f035b6010728a8d2c3d753007b9dc
ruthgabriellep <153202236+ruthgabriellep@users.noreply.github.com>
2026-02-09 15:56:15 +0000

    Dialogue trigger script

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")] 
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")] [SerializeField] private TextAsset InkJSON;
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+            if (InputManager.GetInstance().GetInteractPressed())
+                 
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        playerInRange = false;
+    }
+}
