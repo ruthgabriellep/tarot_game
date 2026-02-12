e3cd237d8b702b7e98bad96b1f909852fd0d0598
ruthgabriellep <153202236+ruthgabriellep@users.noreply.github.com>
2026-02-12 19:05:47 +0000

    Added dialogue panel, not moving yet

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,53 +3,57 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // private PlayerInput playerInput;
+    private PlayerInput playerInput;
 
-    // private InputAction interactAction;
-    // private void Awake()
-    // {
-    //     playerInput = GetComponent<PlayerInput>();
-    //     interactAction = playerInput.actions["Interact"];
-    //     interactAction.ReadValue<float>();
-    // }
-    
-    private InputSystem_Actions inputSystem_Actions;
-    
+    private InputAction interactAction;
+
+    private InputAction moveAction;
     private void Awake()
     {
-        inputSystem_Actions = new InputSystem_Actions();
-    }
-    
-    private void OnEnable()
-    {
-        inputSystem_Actions.Enable();
-    }
-    
-    private void OnDisable()
-    {
-        inputSystem_Actions.Disable();
-        inputSystem_Actions.Player.Move.started -= Move;
-    }
-    
-    void Start()
-    {
-        inputSystem_Actions.Player.Move.started += Move;
-        inputSystem_Actions.Player.Move.performed += Move;
-        inputSystem_Actions.Player.Move.canceled += Move;
+        playerInput = GetComponent<PlayerInput>();
+        interactAction = playerInput.actions["Interact"];
+        interactAction.ReadValue<float>();
         
-        inputSystem_Actions.Player.Move.started += context => Move(context);
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Interact"];
+        moveAction.ReadValue<float>();
     }
     
-    public void Move(InputAction.CallbackContext context)
-    {
-        Debug.Log("Move");
-    }
-    
-    private void Update()
-    {
-        Vector2 move = inputSystem_Actions.Player.Move.ReadValue<Vector2>();
-        Debug.Log(move);
-        if (inputSystem_Actions.Player.Move.triggered)
-            Debug.Log("Move");
-    }
+    //
+    // private InputSystem_Actions inputSystem_Actions;
+    //
+    // private void Awake()
+    // {
+    //     inputSystem_Actions = new InputSystem_Actions();
+    // }
+    //
+    // private void OnEnable()
+    // {
+    //     inputSystem_Actions.Enable();
+    // }
+    //
+    // private void OnDisable()
+    // {
+    //     inputSystem_Actions.Disable();
+    //     inputSystem_Actions.Player.Move.started -= Move;
+    // }
+    //
+    // void Start()
+    // {
+    //     inputSystem_Actions.Player.Move.started += Move;
+    //     inputSystem_Actions.Player.Move.performed += Move;
+    //     inputSystem_Actions.Player.Move.canceled += Move;
+    //     
+    //     inputSystem_Actions.Player.Move.started += context => Move(context);
+    // }
+    //
+    // public void Move(InputAction.CallbackContext context)
+    // {
+    //     Debug.Log("Move");
+    // }
+    //
+    // private void Update()
+    // {
+    //     Vector2 move = inputSystem_Actions.Player.Move.ReadValue<Vector2>();
+    // }
 }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -6,7 +6,7 @@ public class DialogueTrigger : MonoBehaviour
     [SerializeField] private GameObject visualCue;
 
     [Header("Ink JSON")] 
-    [SerializeField] private TextAsset InkJSON;
+    [SerializeField] private TextAsset inkJSON;
     private bool playerInRange;
 
     private void Awake()
@@ -20,6 +20,10 @@ public class DialogueTrigger : MonoBehaviour
         if (playerInRange)
         {
             visualCue.SetActive(true);
+            if (InputManager.GetInstance()GetInteractPressed())
+            {
+                Debug.Log(inkJSON.text);
+            }
         }
         else
         {
