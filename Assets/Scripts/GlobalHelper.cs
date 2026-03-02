fae749ff1c4a0701f718940b3d99ddd6e7a3a353
ruthgabriellep <153202236+ruthgabriellep@users.noreply.github.com>
2026-03-02 15:23:58 +0000

    Added interactable variant, accidentally deleted wrong file (interactable trigger)

--- Assets/Scripts/GlobalHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class GlobalHelper
-{
-    public static string GenerateUniqueID(GameObject obj)
-    {
-        return $"{obj.scene.name}_{obj.transform.position.x}_{obj.transform.position.y}";
-    }
-}
